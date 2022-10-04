5c2cb7562b52fae40b14ebec80bdad7b191cd83c
Mrcsbda <macbedoya@gmail.com>
2022-10-04 16:44:32 -0500

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'RandomAdvices';
+export class AppComponent implements OnInit {
+  constructor(private Http: HttpClient) {}
+
+  advices: Advices|undefined;
+
+  ngOnInit(): void {
+    this.getAdvices();
+  }
+
+  getAdvices() {
+    this.Http.get<any>('https://api.adviceslip.com/advice').subscribe(
+      (advices) => {
+      this.advices = advices.slip;
+      }
+    );
+  }
+
+  newAdvice(){
+    this.getAdvices();
+    console.log('hola')
+  }
+}
+
+interface Advices {
+  id: number;
+  advice: string;
 }
